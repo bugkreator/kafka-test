3b9906d28f69c05e1c3340ef2dc4d3ecb07bc164
unknown <orotem@7SSTWZ1.agoda.local>
2015-05-27 14:54:51 +0700

    New consumer

---
+++ src/main/scala/mykafka/TestConsumer.scala
@@ -0,0 +1,28 @@
+package mykafka
+
+import java.text.SimpleDateFormat
+import java.util.{Calendar, UUID}
+
+import kafka.utils.Logging
+
+object TestConsumer extends Logging {
+   def main(args: Array[String]) {
+
+      info ("Starting...")
+      val producer = new KafkaProducer[String,String](Settings.topicName, Settings.brokerList)
+
+      for (i<-1 to 500)  {
+         val message = "Message " + i + " " + (new SimpleDateFormat("YYYY-MM-dd hh:mm:ss.SSS")).format(Calendar.getInstance.getTime)
+         val key : String = UUID.randomUUID().toString()
+         producer.send(message,key)
+         //Thread.sleep(200)
+         if (i%100==0)
+         {
+            info("Message #" + i)
+         }
+      }
+      producer.close()
+
+      info ("Done.");
+   }
+}

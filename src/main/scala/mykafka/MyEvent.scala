752100d8ac7796676b15ff2fdac1ad3975df8b62
unknown <orotem@7SSTWZ1.agoda.local>
2015-06-26 15:53:34 +0700

    added timestamp field to json

--- src/main/scala/mykafka/MyEvent.scala
+++ src/main/scala/mykafka/MyEvent.scala
@@ -4,6 +4,7 @@ package mykafka
  * Created by orotem on 6/24/2015.
  */
 
+import java.text.SimpleDateFormat
 import java.util.{UUID,Calendar}
 
 import com.fasterxml.jackson.annotation.JsonProperty
@@ -12,14 +13,16 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize
 @JsonSerialize
 class MyEvent(Name: String, Data : Map[String, String] = Map(), Time: Long = Calendar.getInstance.getTimeInMillis(), Id : String = UUID.randomUUID().toString())
 {
-   @JsonProperty("Name")
+   @JsonSerialize
    val name: String = Name
-   @JsonProperty("Data")
+   @JsonSerialize
    val data: Map[String, String] = Data
-   @JsonProperty("Time")
+   @JsonSerialize
    val time: Long = Time
-   @JsonProperty("Id")
+   @JsonSerialize
    val id : String = Id
+   @JsonProperty("timestamp")
+   def timestamp : String = (new SimpleDateFormat("YYYY-MM-dd HH:mm:ss")).format(time)
 
    //TODO: Horribly inefficient. Make object reusable?
 }
--- src/main/scala/mykafka/TestProducer.scala
+++ src/main/scala/mykafka/TestProducer.scala
@@ -12,7 +12,7 @@ object TestProducer extends Logging {
       info ("Starting...")
       val producer = new KafkaProducer[String,String](Settings.topicName, Settings.brokerList, synchronously = false)
 
-      val numMessages : Int =  if (args.length>0) Integer.parseInt(args(0)) else 5000
+      val numMessages : Int =  if (args.length>0) Integer.parseInt(args(0)) else 10000
       val mapper: ObjectMapper = new ObjectMapper()
       mapper.registerModule(DefaultScalaModule)
 
@@ -31,8 +31,9 @@ object TestProducer extends Logging {
                info("Message #" + i)
             }
       }
-      producer.close()
       val endTime:Long = Calendar.getInstance().getTimeInMillis()
+      producer.close()
+
 
       info ("Done - " + numMessages + " messages in " + (endTime - startTime).toString() + " msec.")
    }
